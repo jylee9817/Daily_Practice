ae3e05e101adecde977ff73f42109e3dcc5dca87
Juyeong (이주영) <Osquid45@gmail.com>
2017-02-13 16:38:41 -0800

    selection sorting algorithm was added

--- Pancake_Glutton.cpp
+++ Pancake_Glutton.cpp
@@ -24,3 +24,21 @@ int main()
   getline (std::cin, name);
   std::cout << "Hello, " << name << "!\n";
 }
+
+/*
+The algorithm works by selecting the smallest unsorted item and then swapping it with the item in the next position to be filled.
+The selection sort works as follows: you look through the entire array for the smallest element, once you find it you swap it (the smallest element) 
+with the first element of the array. Then you look for the smallest element in the remaining array (an array without the first element) and swap it with the second element. 
+Then you look for the smallest element in the remaining array (an array without first and second elements) and swap it with the third element, and so on.
+*/
+void selectionSort(int[] ar)
+{
+   for (int i = 0; i ‹ ar.length-1; i++)
+   {
+      int min = i;
+      for (int j = i+1; j ‹ ar.length; j++)
+            if (ar[j] ‹ ar[min]) min = j;
+      int temp = ar[i];
+      ar[i] = ar[min];
+      ar[min] = temp;
+} }
